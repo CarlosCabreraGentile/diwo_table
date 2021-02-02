2b3e365c9b79e26d95ff32efbd6d9be9a8750b07
Carlos Cabrera Gentile <carloscgentile@gmail.com>
2021-02-02 19:03:13 -0300

    table wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diwo-table';
+
+  items = [' ', ' ', ' '];
+  items1 = ['Product Type', 'Target Cardholders', 'Target Segments', 'Sales Amount', 'Absolute', 'Significance', 'Impact'];
+  items2 = ['Consumer', '1,501,231', '540', '6.4%', '$51.21', '99%', '$95M'];
+  rows = [' ',' ', ' '];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
